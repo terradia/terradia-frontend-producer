6392363f110456db6f2794e24709d3e3b0c34d69
Victor <victor.heim@epitech.eu>
2020-01-25 16:31:42 -0800

    feat (init): initializing repo with te basic config and app  pages

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="/components"/>
+/// <reference types="/interfaces"/>
+/// <reference types="/GraphQl"/>
+/// <reference types="/public"/>
