750a3b868710ec65a7972e9705be971287cb3610
Florian <florian.picard15@gmail.com>
2020-11-26 17:39:22 +0100

    feat(graph): month and week grpah

---
+++ src/components/Statistics/StatisticGraph.tsx
@@ -0,0 +1,189 @@
+import React, { useState } from "react";
+import "../../assets/Style/Statistics/Statistics.less";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  LabelList,
+  Tooltip,
+} from "recharts";
+import moment from "moment";
+import TerradiaLoader from "../TerradiaLoader";
+
+interface Props {
+  graphScope: string;
+  data: any;
+  fromDate: Date;
+}
+
+const StatisticGraph = (props: Props) => {
+  const [init, setinit] = useState(true);
+  const [weekOrder, setWeekOrders] = useState([]);
+  const [monthsOrder, setMonthsOrder] = useState([]);
+  // const [yearOrder, setYearOrder] = useState([]);
+  let weekDay = moment().subtract(7, "days").format("DD MMM");
+  let monthDay = moment().subtract(31, "days").format("DD MMM");
+  // let yearMonth = moment().subtract(12, "months");
+  const today = moment().format("DD MMM");
+
+  const getOrdersInWeek = (data) => {
+    setinit(false);
+    let tmpListOrder = [];
+
+    let isCreated = false;
+    if (isCreated === false) {
+      while (weekDay !== today) {
+        tmpListOrder = [
+          ...tmpListOrder,
+          {
+            nb: 0,
+            date: moment(new Date(weekDay)).format("DD MMM"),
+          },
+        ];
+        weekDay = moment(new Date(weekDay)).add(1, "days").format("DD MMM");
+      }
+      isCreated = true;
+    }
+
+    data.forEach((order) => {
+      tmpListOrder.forEach((tmpOrder) => {
+        let tmpNb = 0;
+        if (
+          moment(new Date(tmpOrder.date)).format("DD MMM") ===
+          moment(new Date(order.createdAt)).format("DD MMM")
+        ) {
+          tmpNb = tmpOrder.nb + 1;
+
+          tmpOrder.nb = tmpNb;
+          tmpOrder.date = moment(new Date(order.createdAt)).format("DD MMM");
+        }
+      });
+    });
+
+    setWeekOrders(tmpListOrder);
+  };
+
+  const getOrdersInMonth = (data) => {
+    setinit(false);
+    let tmpListOrder = [];
+
+    let isCreated = false;
+    if (isCreated === false) {
+      while (monthDay !== today) {
+        tmpListOrder = [
+          ...tmpListOrder,
+          {
+            nb: 0,
+            date: moment(new Date(monthDay)).format("DD MMM"),
+          },
+        ];
+        monthDay = moment(new Date(monthDay)).add(1, "days").format("DD MMM");
+      }
+      isCreated = true;
+    }
+
+    data.forEach((order) => {
+      tmpListOrder.forEach((tmpOrder) => {
+        let tmpNb = 0;
+        if (
+          moment(new Date(tmpOrder.date)).format("DD MMM") ===
+          moment(new Date(order.createdAt)).format("DD MMM")
+        ) {
+          tmpNb = tmpOrder.nb + 1;
+
+          tmpOrder.nb = tmpNb;
+          tmpOrder.date = moment(new Date(order.createdAt)).format("DD MMM");
+        }
+      });
+    });
+    setMonthsOrder(tmpListOrder);
+  };
+
+  // const getOrdersInYear = (data) => {
+  //   setinit(false);
+  //   let tmpListOrder = [];
+
+  //   let isCreated = false;
+  //   if (isCreated === false) {
+  //     console.log("yearMonth", yearMonth, today);
+  //   }
+  //   while (yearMonth !== todayMonth) {
+  //     tmpListOrder = [
+  //       ...tmpListOrder,
+  //       {
+  //         nb: 0,
+  //         date: moment(new Date(yearMonth)).format("DD MMM"),
+  //       },
+  //     ];
+  //     yearMonth = moment(new Date(yearMonth))
+  //       .add(1, "months")
+  //       .format("DD MMM");
+  //     console.log("yearMonth YEAR:", yearMonth);
+  //   }
+  //   isCreated = true;
+  // }
+  // data.forEach((order) => {
+  //   tmpListOrder.forEach((tmpOrder) => {
+  //     let tmpNb = 0;
+  //     if (
+  //       moment(new Date(tmpOrder.date)).format("DD MMM") ===
+  //       moment(new Date(order.createdAt)).format("DD MMM")
+  //     ) {
+  //       tmpNb = tmpOrder.nb + 1;
+
+  //       tmpOrder.nb = tmpNb;
+  //       tmpOrder.date = moment(new Date(order.createdAt)).format("DD MMM");
+  //     }
+  //   });
+  // });
+  // console.log("tmpListOrder:", tmpListOrder);
+  //   setYearOrder(tmpListOrder);
+  // };
+
+  if (init === true) {
+    getOrdersInWeek(props.data);
+    getOrdersInMonth(props.data);
+    // getOrdersInYear(props.data);
+    return <TerradiaLoader />;
+  }
+
+  return (
+    <>
+      {props.graphScope === "week" && (
+        <BarChart width={700} height={400} data={weekOrder}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Bar dataKey="nb" fill="#52c41a">
+            <LabelList dataKey="nb" position="center" />
+          </Bar>
+        </BarChart>
+      )}
+      {props.graphScope === "months" && (
+        <BarChart width={700} height={400} data={monthsOrder}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="nb" fill="#52c41a">
+            <LabelList dataKey="nb" position="center" />
+          </Bar>
+        </BarChart>
+      )}
+      {/* {props.graphScope === "year" && (
+        <BarChart width={600} height={400} data={yearOrder}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis />
+          <Bar dataKey="nb" fill="#52c41a">
+            <LabelList dataKey="nb" position="center" />
+          </Bar>
+        </BarChart>
+      )} */}
+    </>
+  );
+};
+
+export default StatisticGraph;
--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,304 +1,193 @@
 import React, { useState } from "react";
-import { Card, Progress, Rate, Statistic, Radio } from "antd";
-import { LikeOutlined } from "@ant-design/icons";
+import { Card, Radio } from "antd";
 import { loader as graphqlLoader } from "graphql.macro";
-import { useQuery } from "@apollo/react-hooks";
+
 import "../assets/Style/Statistics/Statistics.less";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  LabelList,
-  LineChart,
-  Line,
-} from "recharts";
+import StatisticCard from "../components/Statistics/StatisticCard";
+import { useTranslation } from "react-i18next";
+import TerradiaLoader from "../components/TerradiaLoader";
+import moment from "moment";
+import { useLazyQuery, useQuery } from "@apollo/react-hooks";
+import StarReview from "../components/Review/StarReview";
+import StatisticGraph from "../components/Statistics/StatisticGraph";
 
-const data = [
-  {
-    name: "Jan.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Fev.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Mar.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Avr.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Mai",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Jui.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Jui.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Aou.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Sep.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Oct.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Nov.",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-  {
-    name: "Dec",
-    pv: Math.floor(Math.random() * (97 - 2 + 1)) + 2,
-  },
-];
-const data2 = [
-  {
-    name: "Semaine 1",
-    pv: Math.floor(Math.random() * (54 - 13 + 1)) + 13,
-  },
-  {
-    name: "Semaine 2",
-    pv: Math.floor(Math.random() * (54 - 13 + 1)) + 13,
-  },
-  {
-    name: "Semaine 3",
-    pv: Math.floor(Math.random() * (54 - 13 + 1)) + 13,
-  },
-  {
-    name: "Semaine 4",
-    pv: Math.floor(Math.random() * (54 - 13 + 1)) + 13,
-  },
-];
+const getCompanyById = graphqlLoader("../graphql/query/getCompanyById.graphql");
 
-const data3 = [
-  {
-    name: "Lun",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-  {
-    name: "Mar.",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-  {
-    name: "Mer.",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-  {
-    name: "Jeu.",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-  {
-    name: "Ven.",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-  {
-    name: "Sam.",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-  {
-    name: "Dim.",
-    pv: Math.floor(Math.random() * (14 - 1 + 1)) + 1,
-  },
-];
+const getCurrentOrders = graphqlLoader(
+  "../graphql/query/orders/getCurrentOrders.graphql"
+);
 
-const monthsRevenue = Math.floor(Math.random() * (1568 - 125 + 1)) + 125;
-const monthsCommands = Math.floor(Math.random() * (87 - 5 + 1)) + 87;
-
-const getCompanyReviews = graphqlLoader(
-  "../graphql/query/getCompanyReviews.graphql"
+const getCompanyOrderHistories = graphqlLoader(
+  "../graphql/query/orders/getCompanyOrderHistories.graphql"
 );
 
 const Statistics = () => {
+  const { t } = useTranslation("common");
   const companyId = localStorage.getItem("selectedCompany");
 
   const {
-    loading,
-    error: errorCompanyReviews,
-    data: dataCompanyReviews,
-  } = useQuery(getCompanyReviews, {
-    variables: { id: companyId, limit: 10, offset: 1 },
+    loading: loadingGetCompanyById,
+    error: errorGetCompanyById,
+    data: dataCompany,
+  } = useQuery(getCompanyById, {
+    variables: { companyId },
+    fetchPolicy: "cache-first",
   });
 
-  function rateNumber() {
-    let nbReviews = 0;
-    dataCompanyReviews.getCompanyReviews.forEach((user) => {
-      if (user.customerMark) {
-        nbReviews += 1;
-      }
-    });
-    if (nbReviews !== 0) {
-      return <>({nbReviews}) </>;
+  const { loading: loadingOrders, error: errorOrders, data: orders } = useQuery(
+    getCurrentOrders,
+    {
+      variables: { companyId },
     }
-    return nbReviews;
-  }
+  );
 
-  const commentUser = () => {
-    let tmpNbReviews = 0;
-    let ratingTotal = 0;
-    dataCompanyReviews.getCompanyReviews.forEach((user) => {
-      if (user.customerMark) {
-        tmpNbReviews += 1;
-        ratingTotal += user.customerMark;
-      }
-    });
-    if (tmpNbReviews !== 0 && ratingTotal !== 0) {
-      return ratingTotal / tmpNbReviews;
-    }
-    return 0;
-  };
+  const [
+    loadOrderHistories,
+    {
+      loading: loadingOrderHistories,
+      data: orderHistoriesData,
+      // fetchMore: fetchMoreOrderHistories,
+    },
+  ] = useLazyQuery(getCompanyOrderHistories);
 
-  const [graphScope, setGraphScope] = useState("year");
+  const [graphScope, setGraphScope] = useState("week");
+
+  const today = moment();
 
   const onChange = (event) => {
+    // if (event === "year") {
+    //   loadOrderHistories({
+    //     variables: {
+    //       companyId,
+    //       fromDate: moment().subtract(1, "year").toDate(),
+    //       toDate: today,
+    //     },
+    //   });
+    // }
     setGraphScope(event.target.value);
   };
 
-  if (loading) return <div>loading</div>;
+  const getCurrentOrdersNumber = () => {
+    return orders
+      ? orders.getCurrentOrders
+        ? orders.getCurrentOrders.length
+        : console.log(errorOrders)
+      : console.log(errorOrders);
+  };
+
+  const getUniqueCustomer = () => {
+    const uniqueCustomer = new Set(
+      orders.getCurrentOrders.map((order) => order.customer.id)
+    ).size;
+
+    return uniqueCustomer;
+  };
+
+  const getAveragePrice = () => {
+    let tmpTotalPrice = 0;
+    let nbOrders = 0;
+    let averagePrice = 0;
+
+    if (orderHistoriesData)
+      orderHistoriesData.getCompanyOrderHistories.forEach((order) => {
+        tmpTotalPrice += order.price;
+        nbOrders++;
+      });
+    else return;
+    if (tmpTotalPrice && nbOrders !== 0)
+      averagePrice = tmpTotalPrice / nbOrders;
+    return Math.round((averagePrice + Number.EPSILON) * 100) / 100;
+  };
+
+  const getAverageProductsPerOrder = () => {
+    let tmpNbProducts = 0;
+    let nbOrders = 0;
+    let averagePrice = 0;
+
+    if (orderHistoriesData)
+      orderHistoriesData.getCompanyOrderHistories.forEach((order) => {
+        tmpNbProducts += order.numberProducts;
+        nbOrders++;
+      });
+    if (tmpNbProducts || nbOrders !== 0)
+      averagePrice = tmpNbProducts / nbOrders;
+    return Math.round((averagePrice + Number.EPSILON) * 100) / 100;
+  };
+
+  if (loadingOrders || loadingGetCompanyById || loadingOrderHistories)
+    return <TerradiaLoader />;
   return (
     <>
+      {orderHistoriesData
+        ? null
+        : loadOrderHistories({
+            variables: {
+              companyId,
+              fromDate: moment().subtract(31, "days").toDate(),
+              toDate: today,
+            },
+          })}
       <div className={"statistic-page"}>
-        <div className={"strong-statistics"}>
-          <div className={"head-statistics"}>
-            <Card title="Commandes en cours" className={"statistic-card"}>
-              <div className={"description-card"}>
-                <p className={"green-stats"}>8</p>
-                <p>Commandes sont en attentes de prépatation ou de livraison</p>
-              </div>
-            </Card>
-            <Card title="Clients uniques" className={"statistic-card"}>
-              <div className={"description-card"}>
-                <p className={"green-stats"}>4</p>
-                <p>Clients differents qui ont commandé chez vous</p>
-              </div>
-            </Card>
-            <Card title={"Avis"} className={"statistic-card"}>
-              <Card.Grid hoverable={false} className={"grid-style"}>
-                {!errorCompanyReviews && (
-                  <Rate
-                    disabled
-                    defaultValue={2}
-                    allowHalf
-                    value={commentUser()}
-                    className={"rate"}
-                  />
-                )}
-                {rateNumber()}
-              </Card.Grid>
-              <Card.Grid hoverable={false} className={"grid-style"}>
-                <Statistic
-                  title="Entreprise favorite"
-                  value={1128}
-                  prefix={<LikeOutlined />}
-                />
-              </Card.Grid>
-            </Card>
-          </div>
-          <Card
-            title="Commandes complétées"
-            extra={
-              <Radio.Group
-                defaultValue="year"
-                onChange={onChange}
-                buttonStyle="solid"
-              >
-                <Radio.Button value="year">Année</Radio.Button>
-                <Radio.Button value="months">Mois</Radio.Button>
-                <Radio.Button value="week">Semaine</Radio.Button>
-              </Radio.Group>
+        <div className={"head-statistics"}>
+          <StatisticCard
+            title={t("StatisticsPage.currentOrders")}
+            number={getCurrentOrdersNumber()}
+            description={t("StatisticsPage.currentOrdersDefinition")}
+          />
+          <StatisticCard
+            title={t("StatisticsPage.uniqueClients")}
+            number={getUniqueCustomer()}
+            description={t("StatisticsPage.uniqueClientsDescription")}
+          />
+          <StarReview
+            averageMark={
+              dataCompany
+                ? dataCompany.getCompany.averageMark
+                : console.log(errorGetCompanyById)
             }
-            className={"statistic-card"}
-          >
-            <div className={"statistic-graph"}>
-              {graphScope === "year" && (
-                <BarChart width={600} height={400} data={data}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Bar dataKey="pv" fill="#52c41a">
-                    <LabelList dataKey="pv" position="center" />
-                  </Bar>
-                </BarChart>
-              )}
-              {graphScope === "months" && (
-                <BarChart width={500} height={300} data={data2}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Bar dataKey="pv" fill="#52c41a">
-                    <LabelList dataKey="pv" position="center" />
-                  </Bar>
-                </BarChart>
-              )}
-              {graphScope === "week" && (
-                <BarChart width={500} height={300} data={data3}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Bar dataKey="pv" fill="#52c41a">
-                    <LabelList dataKey="pv" position="center" />
-                  </Bar>
-                </BarChart>
-              )}
-            </div>
-          </Card>
-          <Card title="Visites page Producteur" className={"statistic-card"}>
-            <div className={"statistic-graph"}>
-              <LineChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                  top: 5,
-                  right: 30,
-                  left: 20,
-                  bottom: 5,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Line type="monotone" dataKey="pv" stroke="#52c41a" />
-              </LineChart>
-            </div>
-          </Card>
-          <div>
-            <Card title="Produits" className={"statistic-card"}>
-              <div
-                style={{
-                  display: "flex",
-                }}
-              >
-                <Card.Grid hoverable={false} className={"grid-style"}>
-                  <Card.Meta title={"Produits disponibles"} />
-                  <Progress percent={84} size="small" />
-                </Card.Grid>
-                <Card.Grid hoverable={false} className={"grid-style"}>
-                  <Statistic title="Produit favori" value={"Tomate"} />
-                </Card.Grid>
-                <Card.Grid hoverable={false} className={"grid-style"}>
-                  <Card.Meta title={`Revenus ${monthsRevenue}€`} />
-                  <p>
-                    Vous avez completez {monthsCommands} commandes durant le
-                    mois de Septembre
-                  </p>
-                </Card.Grid>
-              </div>
-            </Card>
+            numberOfMarks={dataCompany.getCompany.numberOfMarks}
+          />
+        </div>
+        <Card
+          title={t("StatisticsPage.completedOrders")}
+          extra={
+            <Radio.Group
+              defaultValue="week"
+              onChange={onChange}
+              buttonStyle="solid"
+            >
+              {/* <Radio.Button value="year">Année</Radio.Button> */}
+              <Radio.Button value="months">
+                {t("StatisticsPage.month")}
+              </Radio.Button>
+              <Radio.Button value="week">
+                {t("StatisticsPage.week")}
+              </Radio.Button>
+            </Radio.Group>
+          }
+          className={"statistic-card"}
+        >
+          <div className={"statistic-graph"}>
+            {orderHistoriesData && (
+              <StatisticGraph
+                graphScope={graphScope}
+                data={orderHistoriesData.getCompanyOrderHistories}
+                fromDate={moment().subtract(1, "week").toDate()}
+              />
+            )}
           </div>
+        </Card>
+        <div className={"body-statistics"}>
+          <StatisticCard
+            title={t("StatisticsPage.averagePrice")}
+            number={getAveragePrice()}
+            description={t("StatisticsPage.averagePriceDefinition")}
+          />
+          <StatisticCard
+            title={t("StatisticsPage.averageProductsInOrder")}
+            number={getAverageProductsPerOrder()}
+            description={t("StatisticsPage.averageProductsInOrderDescription")}
+          />
         </div>
       </div>
     </>
