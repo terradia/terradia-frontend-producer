ab920f93197ef03a906ad01e4412046c74ffaf62
Victor <victor.heim@epitech.eu>
2020-11-30 19:59:01 +0100

    feat(orderHistory): modify the grouping logic

--- src/components/Statistics/StatisticGraph.tsx
+++ src/components/Statistics/StatisticGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "../../assets/Style/Statistics/Statistics.less";
 import {
   BarChart,
@@ -9,8 +9,9 @@ import {
   LabelList,
   Tooltip,
 } from "recharts";
-import moment from "moment";
 import TerradiaLoader from "../TerradiaLoader";
+import _ from "lodash";
+import moment from "moment";
 
 interface Props {
   graphScope: string;
@@ -18,152 +19,140 @@ interface Props {
   fromDate: Date;
 }
 
+const UnitOption = {
+  ADD: 1,
+  BOUND: 2,
+  DISPLAY: 3,
+  NB: 4,
+};
+
 const StatisticGraph = (props: Props) => {
-  const [init, setinit] = useState(true);
-  const [initMonth, setinitMonth] = useState(true);
+  const [init, setinit] = useState(false);
   const [weekOrder, setWeekOrders] = useState([]);
   const [monthsOrder, setMonthsOrder] = useState([]);
-  // const [yearOrder, setYearOrder] = useState([]);
-  // let yearMonth = moment().subtract(12, "months");
-  const today = moment().format("DD MMM");
+  const [yearOrders, setYearOrders] = useState([]);
 
-  const getOrdersInWeek = (data) => {
-    setinit(false);
-    let tmpListOrder = [];
-
-    let isCreated = false;
-    if (isCreated === false) {
-      for (
-        let weekDay = moment().subtract(7, "days").format("DD MMM");
-        weekDay !== today;
-        weekDay = moment(new Date(weekDay)).add(1, "days").format("DD MMM")
-      ) {
-        tmpListOrder = [
-          ...tmpListOrder,
-          {
-            nb: 0,
-            date: moment(new Date(weekDay)).format("DD MMM"),
-          },
-        ];
-      }
-      isCreated = true;
-    }
-
-    data.forEach((order) => {
-      tmpListOrder.forEach((tmpOrder) => {
-        if (
-          moment(new Date(tmpOrder.date)).format("DD MMM") ===
-          moment(new Date(order.createdAt)).format("DD MMM")
-        ) {
-          tmpOrder.nb = tmpOrder.nb + 1;
-          tmpOrder.date = moment(new Date(order.createdAt)).format("DD MMM");
+  const getUnitOfTime = (formatFilter, option = undefined) => {
+    switch (formatFilter) {
+      case "D":
+        switch (option) {
+          case UnitOption.ADD:
+            return "d";
+          case UnitOption.BOUND:
+            return "d";
+          case UnitOption.DISPLAY:
+            return "D";
+          case UnitOption.NB:
+            return 7;
+          default:
+            return "YYYY-MM-DD";
         }
-      });
-    });
-
-    setWeekOrders(tmpListOrder);
+      case "MMM":
+        switch (option) {
+          case UnitOption.ADD:
+            return "d";
+          case UnitOption.BOUND:
+            return "d";
+          case UnitOption.DISPLAY:
+            return "DD";
+          case UnitOption.NB:
+            return moment().daysInMonth();
+          default:
+            return "YYYY-MM-DD";
+        }
+      case "YYYY":
+        switch (option) {
+          case UnitOption.ADD:
+            return "M";
+          case UnitOption.BOUND:
+            return "M";
+          case UnitOption.DISPLAY:
+            return "MMM";
+          case UnitOption.NB:
+            return 12;
+          default:
+            return "YYYY-MM";
+        }
+    }
+    return formatFilter;
   };
 
-  const getOrdersInMonth = (data) => {
-    setinitMonth(false);
-    let tmpListOrder = [];
-
-    let isCreated = false;
-    if (isCreated === false) {
-      for (
-        let monthDay = moment().subtract(31, "days").format("DD MMM");
-        monthDay !== today;
-        monthDay = moment(new Date(monthDay)).add(1, "days").format("DD MMM")
-      ) {
-        tmpListOrder = [
-          ...tmpListOrder,
-          {
-            nb: 0,
-            date: moment(new Date(monthDay)).format("DD MMM"),
-          },
-        ];
-      }
-      isCreated = true;
-    }
-
-    data.forEach((order) => {
-      tmpListOrder.forEach((tmpOrder) => {
-        if (
-          moment(new Date(tmpOrder.date)).format("DD MMM") ===
-          moment(new Date(order.createdAt)).format("DD MMM")
-        ) {
-          tmpOrder.nb = tmpOrder.nb + 1;
-          tmpOrder.date = moment(new Date(order.createdAt)).format("DD MMM");
-        }
+  const formatGraphData = useCallback(
+    (formatFilter) => {
+      const startDate = moment().subtract(
+        getUnitOfTime(formatFilter, UnitOption.NB),
+        getUnitOfTime(formatFilter, UnitOption.BOUND)
+      );
+      const endDate = moment();
+      const filters = (item) =>
+        moment(item.updatedAt, "YYYY-MM-DD").format(
+          getUnitOfTime(formatFilter)
+        );
+      let data = _.groupBy(props.data, filters);
+      data = _.map(data, (group, key) => {
+        return {
+          date: key,
+          displayDate: moment(key).format(
+            getUnitOfTime(formatFilter, UnitOption.DISPLAY)
+          ),
+          nb: group.length,
+        };
       });
-    });
-    setMonthsOrder(tmpListOrder);
-  };
+      for (
+        let day = startDate;
+        day <= endDate;
+        day = day.clone().add(1, getUnitOfTime(formatFilter, UnitOption.ADD))
+      ) {
+        if (
+          !_.some(
+            data,
+            (item) => item.date === day.format(getUnitOfTime(formatFilter))
+          )
+        ) {
+          data.push({
+            date: day.format(getUnitOfTime(formatFilter)),
+            displayDate: day.format(
+              getUnitOfTime(formatFilter, UnitOption.DISPLAY)
+            ),
+            nb: undefined,
+          });
+        }
+      }
+      data = _.filter(data, (item) => {
+        return moment(item.date).isBetween(startDate, endDate, undefined, "[]");
+      });
+      return _.sortBy(data, (item) => item.date);
+    },
+    [props.data]
+  );
 
-  // const getOrdersInYear = (data) => {
-  //   setinit(false);
-  //   let tmpListOrder = [];
+  useEffect(() => {
+    setWeekOrders(formatGraphData("D"));
+    setMonthsOrder(formatGraphData("MMM"));
+    setYearOrders(formatGraphData("YYYY"));
+    setinit(true);
+  }, [formatGraphData]);
 
-  //   let isCreated = false;
-  //   if (isCreated === false) {
-  //     console.log("yearMonth", yearMonth, today);
-  //   }
-  //   while (yearMonth !== todayMonth) {
-  //     tmpListOrder = [
-  //       ...tmpListOrder,
-  //       {
-  //         nb: 0,
-  //         date: moment(new Date(yearMonth)).format("DD MMM"),
-  //       },
-  //     ];
-  //     yearMonth = moment(new Date(yearMonth))
-  //       .add(1, "months")
-  //       .format("DD MMM");
-  //     console.log("yearMonth YEAR:", yearMonth);
-  //   }
-  //   isCreated = true;
-  // }
-  // data.forEach((order) => {
-  //   tmpListOrder.forEach((tmpOrder) => {
-  //     let tmpNb = 0;
-  //     if (
-  //       moment(new Date(tmpOrder.date)).format("DD MMM") ===
-  //       moment(new Date(order.createdAt)).format("DD MMM")
-  //     ) {
-  //       tmpNb = tmpOrder.nb + 1;
-
-  //       tmpOrder.nb = tmpNb;
-  //       tmpOrder.date = moment(new Date(order.createdAt)).format("DD MMM");
-  //     }
-  //   });
-  // });
-  // console.log("tmpListOrder:", tmpListOrder);
-  //   setYearOrder(tmpListOrder);
-  // };
-
-  if (init === true || initMonth === true) {
-    getOrdersInWeek(props.data);
-    getOrdersInMonth(props.data);
-    // getOrdersInYear(props.data);
+  if (init === false) {
     return <TerradiaLoader />;
   }
 
   return (
     <>
-      {props.graphScope === "week" && (
+      {props.graphScope === "D" && (
         <BarChart width={700} height={400} data={weekOrder}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
+          <XAxis dataKey="displayDate" />
           <YAxis />
           <Bar dataKey="nb" fill="#52c41a">
             <LabelList dataKey="nb" position="center" />
           </Bar>
         </BarChart>
       )}
-      {props.graphScope === "months" && (
+      {props.graphScope === "MMM" && (
         <BarChart width={700} height={400} data={monthsOrder}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
+          <XAxis dataKey="displayDate" />
           <YAxis />
           <Tooltip />
           <Bar dataKey="nb" fill="#52c41a">
@@ -171,16 +160,17 @@ const StatisticGraph = (props: Props) => {
           </Bar>
         </BarChart>
       )}
-      {/* {props.graphScope === "year" && (
-        <BarChart width={600} height={400} data={yearOrder}>
+      {props.graphScope === "YYYY" && (
+        <BarChart width={700} height={400} data={yearOrders}>
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
+          <XAxis dataKey="displayDate" />
           <YAxis />
+          <Tooltip />
           <Bar dataKey="nb" fill="#52c41a">
             <LabelList dataKey="nb" position="center" />
           </Bar>
         </BarChart>
-      )} */}
+      )}
     </>
   );
 };
